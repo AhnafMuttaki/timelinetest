caa37c54166decdca06933192418c843bd79ce86
Ahnaf Muttaki <ahnafx88@gmail.com>
2021-03-19 15:14:07 +0600

    Delete instance done

--- lib.php
+++ lib.php
@@ -29,3 +29,23 @@ function timelinetest_add_instance($timelinetest) {
     $timelinetest->id = $DB->insert_record('timelinetest', $timelinetest);
     return $timelinetest->id;
 }
+
+/**
+ * Given an ID of an instance of this module,
+ * this function will permanently delete the instance
+ * and any data that depends on it.
+ *
+ * @param int $id the id of the quiz to delete.
+ * @return bool success or failure.
+ */
+function timelinetest_delete_instance($id) {
+    global $DB;
+
+    $DB->delete_records('timelineoptions', array('timelinetestid'=>$id));
+    $DB->delete_records('timelinephases', array('timelinetestid'=> $id));
+    $DB->delete_records('timelineattemptlog', array('timelinetestid'=>$id));
+    $DB->delete_records('timelinetotalmark', array('timelinetestid'=>$id));
+    $DB->delete_records('timelinetest', array('id'=> $id));
+
+    return true;
+}
